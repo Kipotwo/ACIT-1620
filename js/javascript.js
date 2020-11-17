8107e5a105be1524a19cca923c6cfb9982e9fa8a
Aron Yang <ayang100@my.bcit.ca>
2020-11-16 21:41:05 -0800

    Added array of objects called courseList

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+let courseList = [
+    {
+        code: "ACIT 1515"
+        name: "Scripting for IT"
+    },
+    {
+        code: "MATH 1310"
+        name: "Technical Math for IT"
+    },
+    {
+        code: "ACIT 1420"
+        name: "Intro to System Admin"
+    }
+]
