078c3406a7359f100158ec8164c8d92fcca684b4
Aron Yang <ayang100@my.bcit.ca>
2020-11-16 23:54:29 -0800

    Added loops

--- js/javascript.js
+++ js/javascript.js
@@ -1,14 +1,38 @@
 let courseList = [
     {
-        code: "ACIT 1515"
+        code: "ACIT 1515",
         name: "Scripting for IT"
     },
     {
-        code: "MATH 1310"
+        code: "MATH 1310",
         name: "Technical Math for IT"
     },
     {
-        code: "ACIT 1420"
+        code: "ACIT 1420",
         name: "Intro to System Admin"
     }
-]
+];
+
+let user_input = "Placeholder text"
+let length_of_input = 0;
+let course;
+
+do{
+    user_input = prompt("Enter a 4 digit code: ");
+    length_of_input = user_input.length;
+}
+while ((length_of_input !== 4) || (isNaN(user_input) === true))
+  
+for (course in courseList){
+    let code_name = courseList[course].code
+    if (code_name.includes(user_input)){
+        console.log(`Yes I am taking the course: ${courseList[course].code} - ${courseList[course].name}`);
+    }
+    else{
+        new_object = {code: user_input, name: null}
+        courseList.push(new_object)
+        console.log(courseList)
+        console.log(`Successfully added new object with code: ${user_input}`)
+        break
+    }
+}
