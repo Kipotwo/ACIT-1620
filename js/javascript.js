746876a4a7b798b79520a7856a363d3a31651a75
Aron Yang <ayang100@my.bcit.ca>
2020-11-16 23:56:26 -0800

    Revised comments

--- js/javascript.js
+++ js/javascript.js
@@ -21,17 +21,20 @@ do{
     user_input = prompt("Enter a 4 digit code: ");
     length_of_input = user_input.length;
 }
+// Proceeds to run the code until both conditions are met
 while ((length_of_input !== 4) || (isNaN(user_input) === true))
   
 for (course in courseList){
     let code_name = courseList[course].code
+    // Checks to see if user input is in code
     if (code_name.includes(user_input)){
         console.log(`Yes I am taking the course: ${courseList[course].code} - ${courseList[course].name}`);
     }
     else{
+        // If user input is not in any object's code, make a new object
         new_object = {code: user_input, name: null}
         courseList.push(new_object)
-        console.log(courseList)
+        // console.log(courseList)
         console.log(`Successfully added new object with code: ${user_input}`)
         break
     }
