028fdcc449bc4e783b1b61d85b3827bec397987c
Aron Yang <ayang100@my.bcit.ca>
2020-11-24 22:05:04 -0800

    Added function that makes box green

--- js/javascript.js
+++ js/javascript.js
@@ -8,32 +8,38 @@ function createCourseArray(){
         let course = {code: courseCode, date: courseDate}
         courseList.push(course)
     }
-    console.log(courseList)
+    return courseList
 }
-// let user_input = "Placeholder text"
-// let length_of_input = 0;
 
-// do{
-//     user_input = prompt("Enter a 4 digit code: ");
-//     length_of_input = user_input.length;
-// }
-// Proceeds to run the code until both conditions are met
-// while ((length_of_input !== 4) || (isNaN(user_input) === true))
+function findCourse(courseList){
+    let user_input = "Placeholder text"
+    let length_of_input = 0;
+
+    do{
+        user_input = prompt("Enter a 4 digit code: ");
+        length_of_input = user_input.length;
+    }
+    // Proceeds to run the code until both conditions are met
+    while ((length_of_input !== 4) || (isNaN(user_input) === true))
   
-// for (let course in courseList){
-//     let code_name = courseList[course].code
-//     Checks to see if user input is in code
-//     if (code_name.includes(user_input)){
-//         console.log(`Yes I am taking the course: ${courseList[course].code} - ${courseList[course].name}`);
-//     }
-//     else{
-//         If user input is not in any object's code, make a new object
-//         new_object = {code: user_input, name: null}
-//         courseList.push(new_object)
-//         console.log(courseList)
-//         console.log(`Successfully added new object with code: ${user_input}`)
-//         break
-//     }
-// }
+    for (let course in courseList){
+        let code_name = courseList[course].code
+        // Checks to see if user input is in code
+        if (code_name.includes(user_input)){
+            // If so, make the box it that contains it green
+            divBlocks = document.querySelectorAll("div");
+            divBlocks[course].style.backgroundColor = "green";
+        }
+        else{
+            // If user input is not in any object's code, make a new object
+            new_object = {code: user_input, name: null}
+            courseList.push(new_object)
+            console.log(courseList)
+            console.log(`Successfully added new object with code: ${user_input}`)
+            break
+        }
+    }
+}
 
-createCourseArray()
+courseList = createCourseArray()
+findCourse(courseList)
